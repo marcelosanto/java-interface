f6988eeaed63866635df3384e18b0899b64ab853
Marcelo Santos <rocha.mer21@gmail.com>
2021-06-26 09:48:36 -0300

    criado algumas classes principais

---
+++ src/model/services/RentalServices.java
@@ -0,0 +1,44 @@
+package model.services;
+
+
+import model.entities.CarRental;
+
+public class RentalServices {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private BrazilTaxService taxService;
+
+    public RentalServices(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public Double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(Double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public Double getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(Double pricePerHour) {
+        this.pricePerHour = pricePerHour;
+    }
+
+    public BrazilTaxService getTaxService() {
+        return taxService;
+    }
+
+    public void setTaxService(BrazilTaxService taxService) {
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental){}
+}
