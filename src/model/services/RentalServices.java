5d7379989969a6a2514ce09543747011e39ab6c0
Marcelo Santos <rocha.mer21@gmail.com>
2021-06-26 12:21:42 -0300

    exemplo resolvido sem uso de Interface

--- src/model/services/RentalServices.java
+++ src/model/services/RentalServices.java
@@ -2,6 +2,7 @@ package model.services;
 
 
 import model.entities.CarRental;
+import model.entities.Invoice;
 
 public class RentalServices {
 
@@ -40,5 +41,19 @@ public class RentalServices {
         this.taxService = taxService;
     }
 
-    public void processInvoice(CarRental carRental){}
+    public void processInvoice(CarRental carRental){
+        long t1 = carRental.getStart().getTime();
+        long t2 = carRental.getFinish().getTime();
+        double hours = (double) (t2-t1) / 1000/60/60;
+        double basicPayment;
+
+        if (hours <= 12.0){
+            basicPayment = Math.ceil(hours) * pricePerHour;
+        } else {
+            basicPayment = Math.ceil(hours/24) * pricePerDay;
+        }
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
 }
